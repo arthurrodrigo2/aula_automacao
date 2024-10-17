532bbd346c2490d6f7f8329281382952ccf7e632
arthurrodrigo2 <arthurrodrigo2@gmail.com>
2024-10-17 20:06:43 -0300

    Versão 1.0

---
+++ app.py
@@ -0,0 +1,23 @@
+# importando biblioteca de automação
+# há a necessidade de instalação 'pip install pyautogui'
+import pyautogui as auto
+
+auto.PAUSE = 1.0
+
+auto.press("win")
+auto.write("chrome")
+auto.press("enter")
+auto.hotkey("alt", "space")
+auto.press("x")
+auto.write("Python.org")
+auto.press("enter")
+auto.hotkey("ctrl","t")
+auto.write("google.com.br")
+auto.press("enter")
+auto.write("logo python")
+auto.press("enter")
+
+for i in range(14):
+    auto.press("tab")
+
+auto.press("enter")
